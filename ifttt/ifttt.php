dc99fd8faa6f3b61b049e96089985da6d8aad96e
Andrii Kalmus <andrii.kalmus@abysim.com>
2024-06-15 16:59:04 +0300

    category

--- ifttt/ifttt.php
+++ ifttt/ifttt.php
@@ -162,7 +162,7 @@ function ifttt_message($uid, $item)
 	$post['title'] = '';
 	$post['body'] = $item['msg'];
 	if (!empty($item['cat'])) {
-		$post['post-category'] = $item['cat'];
+		$post['category'] = $item['cat'];
 	}
 	//$post['date'] = $item['date'];
 	//$post['uri'] = $item['url'];
